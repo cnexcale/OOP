c57ee7301e9e50bef3890f504abfc4914c86fd7a
ColanNexcale <lukas.r@freenet.de>
2018-06-18 20:49:57 +0200

    Aufgabe 9

--- LandscapeWithManyTrees.java
+++ LandscapeWithManyTrees.java
@@ -15,8 +15,8 @@ class LandscapeWithManyTrees implements TreeDecider{
 		grassZone = maxHeight*(0.6f); //System.out.println("grassZone: "+maxHeight*0.3);
 		
 		if (height > waterZone && height < grassZone && Math.abs(slope) < 2){
-			if (Math.random()*10 > 4.5) return 1;
-			else return 3;
-		}else return 0;         
+			if (Math.random()*10 > 4.5) return CONE_TREE;
+			else return CONE_BUSH;
+		}else return NO_TREE;         
   }
 }
